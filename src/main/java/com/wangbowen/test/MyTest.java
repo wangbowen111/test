e8841bc6c4ff4a386cb5385a2020a4f87c0019f6
wbw <wangbw@qq.com>
2020-03-25 15:32:59 +0800

    second

--- src/main/java/com/wangbowen/test/MyTest.java
+++ src/main/java/com/wangbowen/test/MyTest.java
@@ -3,6 +3,6 @@ package com.wangbowen.test;
 public class MyTest {
 
 	public static void main(String[] args) {
-		System.out.println("helloWord");
+		System.out.println("helloWord  --- git 666");
 	}
 }
